5e29a49dee2d289a0062aad276042988b0bb4e02
Danny Wu <danny.wu@onware.com>
2020-11-06 17:15:53 -0700

    AT-162 Commits for the tableau snapshot.

--- TableauAPI/RESTRequests/DownloadView.cs
+++ TableauAPI/RESTRequests/DownloadView.cs
@@ -72,6 +72,36 @@ namespace TableauAPI.RESTRequests
         }
 
         /// <summary>
+        /// Return an Image for a View with filters.
+        /// </summary>
+        /// <param name="workbookId"></param>
+        /// <param name="viewId"></param>
+        /// <param name="param1"></param>
+        /// <param name="param2"></param>
+        /// <param name="val1"></param>
+        /// <param name="val2"></param>
+        /// <returns></returns>
+        public byte[] GetImageWithFilters(string workbookId, string viewId, string param1, string param2, string val1, string val2, string ticket)
+        {
+            var url = "https://tableau.onware.com/trusted/" + ticket + "/t/AssessmentTool/views/Responses/Snapshot.png?CompanyId=fd59cde3-90f8-463a-8c82-c5c1f030e9ea&ModuleId=c9cb1e8e-ab27-4957-83a9-27f3bbdcdbf5";
+            var webRequest = CreateLoggedInWebRequest(url);
+            webRequest.Method = "GET";
+            var response = GetWebResponseLogErrors(webRequest, "get view image");
+            byte[] thumbnail;
+            using (var stream = response.GetResponseStream())
+            {
+
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    thumbnail = ms.ToArray();
+                }
+            }
+            return thumbnail;
+        }
+
+
+        /// <summary>
         /// Return data for a View (CSV format)
         /// </summary>
         /// <param name="viewId"></param>
